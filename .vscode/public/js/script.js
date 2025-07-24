e61b6733452711e84670ae8d1a1c3fac3511c70a
Your Name <your-email@example.com>
2025-07-24 17:50:05 +0530

    first commit

---
+++ .vscode/public/js/script.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+
+        socket.emit("sendLocation", { latitude, longitude });
+    }, (error) => {
+        console.log(error);
+    }, {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    });
+}
+
+
+Location.map("map");
